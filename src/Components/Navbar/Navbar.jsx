42723cc3f4b45712b9a5f4712c96d0c0ecbc904e
liblissz <liblissz3@gmail.com>
2025-08-05 17:10:09 -0700

    dd

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Link} from 'react'
 import './Navbar.css'
 import navlogo from '../../assets/Admin_Assets/logo.png'
 import navprofile from '../../assets/Admin_Assets/dropdown_icon.png'
@@ -6,10 +6,12 @@ import adminimage from '../../assets/Admin_Assets/product_13.jpg'
 const Navbar = () => {
   return (
     <div className='navbar'>
+      <Link to={'/'}>
 <div className="navbarlogo">
       <img src={navlogo} alt="" className="nava-logo" />
  <h1>BTC PHARMACY <p>ADMIN PANNEL</p></h1>
 </div>
+</Link>
 
       <div className="profile">
         <img src="https://res.cloudinary.com/dbq5gkepx/image/upload/v1753095928/u4lds8txvd9shgnncaqj.jpg" alt="" className='adminimage' />
